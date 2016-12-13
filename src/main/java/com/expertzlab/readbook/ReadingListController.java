09ca20908a0fd50396376d1e60c93fb1e1e12de0
Gireesh Babu <gireesh.babu@mozanta.com>
2016-12-13 08:42:08 +0530

    initial import

---
+++ src/main/java/com/expertzlab/readbook/ReadingListController.java
@@ -0,0 +1,43 @@
+package com.expertzlab.readbook;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+/**
+ * Created by gireeshbabu on 13/12/16.
+ */
+
+@Controller
+@RequestMapping("/")
+public class ReadingListController {
+
+    ReadingListRepo repo;
+
+    @Autowired
+    public ReadingListController(ReadingListRepo repo){
+        this.repo = repo;
+    }
+
+    @RequestMapping(value="/{reader}", method= RequestMethod.GET)
+    public String readersBook(@PathVariable("reader") String reader, Model model){
+        List<Book> bookList = repo.findByReader(reader);
+        if(bookList != null){
+            model.addAttribute("books",bookList);
+        }
+        return "readingList";
+    }
+
+    @RequestMapping(value="/{reader}", method=RequestMethod.POST)
+    public String addToReadingList(
+            @PathVariable("reader") String reader, Book book) {
+        book.setReader(reader);
+        repo.save(book);
+        return "redirect:/{reader}";
+    }
+}
