34d501f5e97597ef71b8d7b38fb988596c11ed12
Gireesh Babu <gireesh.babu@mozanta.com>
2016-12-19 07:59:53 +0530

    changes

--- src/main/java/com/expertzlab/demoapp/HelloController.java
+++ src/main/java/com/expertzlab/demoapp/HelloController.java
@@ -1,7 +1,17 @@
 package com.expertzlab.demoapp;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by gireeshbabu on 28/11/16.
  */
-public class HelloController {
+
+@RestController
+class HelloController {
+
+    @RequestMapping("/first")
+    public String hello() {
+        return "Hello World";
+    }
 }
--- src/main/java/com/expertzlab/demoapp/ReadingListRepository.java
+++ src/main/java/com/expertzlab/demoapp/ReadingListRepository.java
@@ -3,5 +3,9 @@ package com.expertzlab.demoapp;
 /**
  * Created by gireeshbabu on 28/11/16.
  */
-public interface ReadingListRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+public interface ReadingListRepository extends JpaRepository<Book, Long> {
+    List<Book> findByReader(String reader);
 }
