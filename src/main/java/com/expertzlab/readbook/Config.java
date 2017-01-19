7da716999f0fbfb715af64f7c5e6f9473e07c32c
Gireesh Babu <gireesh.babu@mozanta.com>
2017-01-19 17:28:51 +0530

    rescaled images and removed unnessary java files

--- src/main/java/com/expertzlab/readbook/Config.java
+++
@@ -1,7 +0,0 @@
-package com.expertzlab.readbook;
-
-/**
- * Created by gireeshbabu on 13/12/16.
- */
-public class Config {
-}
--- src/main/resources/db/migration/sql/V1__create.user.sql
+++
@@ -1,7 +0,0 @@
-create table user_info(
-  id VARCHAR(100) NOT NULL,
-  first_name varchar(100),
-  last_name varchar(100),
-  birth_date DATE,
-  PRIMARY KEY (id)
-);
